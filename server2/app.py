51ce0935b60e508ad1ccc10b6f3c1b264eb3d4ab
ritvikraj115 <143361673+ritvikraj115@users.noreply.github.com>
2024-09-01 19:12:14 +0530

    Update app.py

--- server2/app.py
+++ server2/app.py
@@ -89,5 +89,6 @@ def process_frame():
         
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
